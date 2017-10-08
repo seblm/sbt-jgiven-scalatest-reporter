92ca0e7d4b0135fa6754170cb93db18c2fc66e84
Sébastian Le Merdy <sebastian.lemerdy@compte-nickel.fr>
2017-10-08 22:57:46 +0200

    Upgrade JGiven version from 0.15.1 to 0.15.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  private val jGivenVersion = "0.15.1"
+  private val jGivenVersion = "0.15.3"
 
   lazy val `jgiven-core` = "com.tngtech.jgiven" % "jgiven-core" % jGivenVersion
   lazy val `jgiven-html5-report` = "com.tngtech.jgiven" % "jgiven-html5-report" % jGivenVersion
