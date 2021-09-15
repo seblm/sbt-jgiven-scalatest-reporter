27bdb780ecf8cbd750a26ebe22f17eaa2c3b3260
Scala Steward <me@scala-steward.org>
2021-09-15 02:24:57 +0200

    Update scala-library, scalatest and scalafmt-core

--- src/sbt-test/sbt-jgiven-scalatest-reporter/simple/project/Dependencies.scala
+++ src/sbt-test/sbt-jgiven-scalatest-reporter/simple/project/Dependencies.scala
@@ -3,6 +3,6 @@ import sbt._
 object Dependencies {
 
   lazy val `log4j-slf4j-impl` = "org.apache.logging.log4j" % "log4j-slf4j-impl" % "2.14.1"
-  lazy val scalatest = "org.scalatest" %% "scalatest" % "3.2.9"
+  lazy val scalatest = "org.scalatest" %% "scalatest" % "3.2.10"
 
 }
