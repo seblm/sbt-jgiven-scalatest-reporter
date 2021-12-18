ee02d23f8f1bc6ed54717abd32523d86f897f9fc
Scala Steward <me@scala-steward.org>
2021-12-18 09:02:58 +0100

    Update log4j-slf4j-impl to 2.17.0

--- src/sbt-test/sbt-jgiven-scalatest-reporter/simple/project/Dependencies.scala
+++ src/sbt-test/sbt-jgiven-scalatest-reporter/simple/project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  lazy val `log4j-slf4j-impl` = "org.apache.logging.log4j" % "log4j-slf4j-impl" % "2.14.1"
+  lazy val `log4j-slf4j-impl` = "org.apache.logging.log4j" % "log4j-slf4j-impl" % "2.17.0"
   lazy val scalatest = "org.scalatest" %% "scalatest" % "3.2.10"
 
 }
