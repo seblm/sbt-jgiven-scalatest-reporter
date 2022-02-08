a40d13329131a37465debff9b6fae5e40de7f141
Scala Steward <me@scala-steward.org>
2022-02-08 19:29:34 +0100

    Update slf4j-api, gson, scalafmt-core, sbt-sonatype and log4j-slf4j-impl

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,11 +4,11 @@ object Dependencies {
 
   private val jGivenVersion = "1.2.0"
 
-  lazy val gson = "com.google.code.gson" % "gson" % "2.8.9"
+  lazy val gson = "com.google.code.gson" % "gson" % "2.9.0"
   lazy val `jgiven-core` = "com.tngtech.jgiven" % "jgiven-core" % jGivenVersion
   lazy val `jgiven-html5-report` = "com.tngtech.jgiven" % "jgiven-html5-report" % jGivenVersion
-  lazy val `log4j-slf4j-impl` = "org.apache.logging.log4j" % "log4j-slf4j-impl" % "2.17.1" % Test
+  lazy val `log4j-slf4j-impl` = "org.apache.logging.log4j" % "log4j-slf4j-impl" % "2.17.2" % Test
   lazy val scalatest = "org.scalatest" %% "scalatest" % "3.2.11"
-  lazy val `slf4j-api` = "org.slf4j" % "slf4j-api" % "1.7.35"
+  lazy val `slf4j-api` = "org.slf4j" % "slf4j-api" % "1.7.36"
 
 }
