c2d6e4edcdb6b1188054b1301c7d02907201b05b
Scala Steward <me@scala-steward.org>
2022-04-25 22:14:19 +0200

    Update scalatest, jgiven-core, jgiven-html5-report, scalafmt-core and sbt-sonatype

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,13 +2,13 @@ import sbt._
 
 object Dependencies {
 
-  private val jGivenVersion = "1.2.0"
+  private val jGivenVersion = "1.2.1"
 
   lazy val gson = "com.google.code.gson" % "gson" % "2.9.0"
   lazy val `jgiven-core` = "com.tngtech.jgiven" % "jgiven-core" % jGivenVersion
   lazy val `jgiven-html5-report` = "com.tngtech.jgiven" % "jgiven-html5-report" % jGivenVersion
   lazy val `log4j-slf4j-impl` = "org.apache.logging.log4j" % "log4j-slf4j-impl" % "2.17.2" % Test
-  lazy val scalatest = "org.scalatest" %% "scalatest" % "3.2.11"
+  lazy val scalatest = "org.scalatest" %% "scalatest" % "3.2.12"
   lazy val `slf4j-api` = "org.slf4j" % "slf4j-api" % "1.7.36"
 
 }
