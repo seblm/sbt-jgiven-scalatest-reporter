f2426b3d0fdf97a03950add6f7aed77a95298966
Scala Steward <scala_steward@virtuslab.com>
2022-08-02 17:43:58 +0000

    Update scalatest to 3.2.13

--- src/sbt-test/sbt-jgiven-scalatest-reporter/simple/project/Dependencies.scala
+++ src/sbt-test/sbt-jgiven-scalatest-reporter/simple/project/Dependencies.scala
@@ -3,6 +3,6 @@ import sbt._
 object Dependencies {
 
   lazy val `log4j-slf4j-impl` = "org.apache.logging.log4j" % "log4j-slf4j-impl" % "2.17.2"
-  lazy val scalatest = "org.scalatest" %% "scalatest" % "3.2.12"
+  lazy val scalatest = "org.scalatest" %% "scalatest" % "3.2.13"
 
 }
