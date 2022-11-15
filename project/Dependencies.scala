5e5080a5726cd4feac1f01267ce5464eabe1952f
Scala Steward <scala_steward@virtuslab.com>
2022-11-15 06:13:51 +0000

    Update scalafmt, slf4j-api and sbt-sonatype

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,6 +9,6 @@ object Dependencies {
   lazy val `jgiven-html5-report` = "com.tngtech.jgiven" % "jgiven-html5-report" % jGivenVersion
   lazy val `log4j-slf4j-impl` = "org.apache.logging.log4j" % "log4j-slf4j-impl" % "2.19.0" % Test
   lazy val scalatest = "org.scalatest" %% "scalatest" % "3.2.14"
-  lazy val `slf4j-api` = "org.slf4j" % "slf4j-api" % "2.0.3"
+  lazy val `slf4j-api` = "org.slf4j" % "slf4j-api" % "2.0.4"
 
 }
