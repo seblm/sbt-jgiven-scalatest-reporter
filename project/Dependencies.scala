98b424f01facf090a872f9c2a93e0773331ee53b
Scala Steward <scala_steward@virtuslab.com>
2022-12-07 09:30:40 +0000

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,13 +2,13 @@ import sbt._
 
 object Dependencies {
 
-  private val jGivenVersion = "1.2.4"
+  private val jGivenVersion = "1.2.5"
 
-  lazy val gson = "com.google.code.gson" % "gson" % "2.10"
+  lazy val gson = "com.google.code.gson" % "gson" % "2.10.1"
   lazy val `jgiven-core` = "com.tngtech.jgiven" % "jgiven-core" % jGivenVersion
   lazy val `jgiven-html5-report` = "com.tngtech.jgiven" % "jgiven-html5-report" % jGivenVersion
-  lazy val `log4j-slf4j-impl` = "org.apache.logging.log4j" % "log4j-slf4j-impl" % "2.19.0" % Test
-  lazy val scalatest = "org.scalatest" %% "scalatest" % "3.2.14"
-  lazy val `slf4j-api` = "org.slf4j" % "slf4j-api" % "2.0.4"
+  lazy val `log4j-slf4j-impl` = "org.apache.logging.log4j" % "log4j-slf4j-impl" % "2.20.0" % Test
+  lazy val scalatest = "org.scalatest" %% "scalatest" % "3.2.15"
+  lazy val `slf4j-api` = "org.slf4j" % "slf4j-api" % "2.0.7"
 
 }
