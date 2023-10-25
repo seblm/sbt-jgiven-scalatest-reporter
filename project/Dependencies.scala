13f688274d0bdbb17f822f62bc5679bb0275d208
Scala Steward <scala_steward@virtuslab.com>
2023-10-25 05:25:34 +0000

    Update log4j-slf4j2-impl to 2.21.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   lazy val gson = "com.google.code.gson" % "gson" % "2.10.1"
   lazy val `jgiven-core` = "com.tngtech.jgiven" % "jgiven-core" % jGivenVersion
   lazy val `jgiven-html5-report` = "com.tngtech.jgiven" % "jgiven-html5-report" % jGivenVersion
-  lazy val `log4j-slf4j-impl` = "org.apache.logging.log4j" % "log4j-slf4j2-impl" % "2.20.0" % Test
+  lazy val `log4j-slf4j-impl` = "org.apache.logging.log4j" % "log4j-slf4j2-impl" % "2.21.1" % Test
   lazy val scalatest = "org.scalatest" %% "scalatest" % "3.2.17"
   lazy val `slf4j-api` = "org.slf4j" % "slf4j-api" % "2.0.9"
 
