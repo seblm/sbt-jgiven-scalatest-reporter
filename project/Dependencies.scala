8c95b72d3e9adbc1b8d05b42f923b7fc3d35a9cb
Scala Steward <scala_steward@virtuslab.com>
2024-02-20 07:21:18 +0000

    Update slf4j-api to 2.0.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,6 +9,6 @@ object Dependencies {
   lazy val `jgiven-html5-report` = "com.tngtech.jgiven" % "jgiven-html5-report" % jGivenVersion
   lazy val `log4j-slf4j-impl` = "org.apache.logging.log4j" % "log4j-slf4j2-impl" % "2.22.1" % Test
   lazy val scalatest = "org.scalatest" %% "scalatest" % "3.2.17"
-  lazy val `slf4j-api` = "org.slf4j" % "slf4j-api" % "2.0.11"
+  lazy val `slf4j-api` = "org.slf4j" % "slf4j-api" % "2.0.12"
 
 }
